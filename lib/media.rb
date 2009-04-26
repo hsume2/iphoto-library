3a9ac98d39eb69f4db17c43742eb59146b95c7ec
hsume2 <henry@qlane.com>
2009-04-26 12:20:33 -0700

    optional append

--- lib/media.rb
+++ lib/media.rb
@@ -2,11 +2,19 @@ class Media < ActiveRecord::Base
   set_table_name "iphoto_medias"
   belongs_to :roll
   
-  def thumb_path
-    IphotoLibrary.server + super
+  def thumb_path(append=true)
+    if append
+      IphotoLibrary.server + super
+    else
+      super
+    end
   end
   
-  def image_path
-    IphotoLibrary.server + super
+  def image_path(append=true)
+    if append
+      IphotoLibrary.server + super
+    else
+      super
+    end
   end
 end
